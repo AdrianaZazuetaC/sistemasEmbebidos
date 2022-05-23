254cc4fc5fc6c2a32af6e069e6c42c3d978119cb
AdrianaZazuetaC <49730413+AdrianaZazuetaC@users.noreply.github.com>
2022-05-22 17:56:57 -0700

    primera asigancion

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sistemasEmbebidos
 Asignaciones de la materia sistemas embebidos
+
+#Asignacion 1: 
+
+-OutPut hello word! 
---
+++ vscode/es/host/assigment-1.c
@@ -0,0 +1,10 @@
+/* 
+Asignacion 1 - Output Hello word
+*/
+#include <stdio.h>
+int main()
+{
+       printf("Hello word ! \n Today is a great day!");
+
+    return 0;
+}
